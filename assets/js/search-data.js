260f3f16cb40024a47dc13a4d1a83e1bd75222d0
Excalibur-0613 <36143969+Excalibur-0613@users.noreply.github.com>
2025-05-08 03:40:34 +0000

    Deploying to gh-pages from @ Excalibur-0613/Excalibur-0613.github.io@2cd16eb0506fff532bb38cf7e1f80fbb2b6b5625 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -383,51 +383,26 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-thesis",
+          title: 'Thesis',
+          description: "Multi-Robot Collaboration via Contact Perception &amp; Non-Prehensile Object Transportation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-force-sensitive-motor-driver",
+          title: 'Force Sensitive Motor Driver',
+          description: "Ongoing development for force responsive joints",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-sim-to-real-space-debris-simulator",
+          title: 'Sim-to-Real Space Debris Simulator',
+          description: "Simulating space debris free motion on earth to understand trajectories mirrored in space",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-cellular-automata-attractor-and-repellor-system",
+          title: 'Cellular Automata attractor and repellor system',
+          description: "A simulation for a complex system with simple rules to move objects to a target region in 2D space",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
